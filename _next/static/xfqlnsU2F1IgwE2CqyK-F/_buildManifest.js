8af9cc4385d59822954c2bdcf21ea67d21ac48a4
Edvin Eriksson <edvin@pokeyo.se>
2022-11-24 19:20:01 +0100

    push web

---
+++ _next/static/xfqlnsU2F1IgwE2CqyK-F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-36b39074e2867c87.js","static/chunks/pages/index-c1bc0ac6b2c2dc15.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
